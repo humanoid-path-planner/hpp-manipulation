13dad80a48ec6a4eff33be1132c5b86cdc00409c
Florent Lamiraux <florent@laas.fr>
2021-01-20 07:11:52 +0000

    Remove spurious debug output.

--- src/manipulation-planner.cc
+++ src/manipulation-planner.cc
@@ -13,7 +13,7 @@
 // General Lesser Public License for more details.  You should have
 // received a copy of the GNU Lesser General Public License along with
 // hpp-manipulation. If not, see <http://www.gnu.org/licenses/>.
-#define HPP_DEBUG
+
 #include "hpp/manipulation/manipulation-planner.hh"
 
 #include <boost/tuple/tuple.hpp>
@@ -200,10 +200,6 @@ namespace hpp {
 	      bool success;
               ConfigurationPtr_t q_new (new Configuration_t
 					(path->eval(t_final, success)));
-	      hppDout(info, pinocchio::displayConfig(*near->configuration()));
-	      hppDout(info, pinocchio::displayConfig(*q_new));
-	      hppDout(info, path);
-	      hppDout(info, *path);
 	      assert(success);
 	      assert(path->constraints()->isSatisfied(*q_new));
 	      assert(problem_->constraintGraph ()->getState(*q_new));
@@ -327,7 +323,6 @@ namespace hpp {
       }
       HPP_STOP_TIMECOUNTER (validatePath);
       if (fullValidPath->length () == 0) {
-	hppDout(info, "");
         es.addFailure (reasons_[FAILURE]);
         es.addFailure (reasons_[PATH_VALIDATION_ZERO]);
         validPath = fullValidPath;

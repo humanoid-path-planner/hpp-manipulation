be8a7add932efe6530029873aef7fd8cf9f7b856
Florent Lamiraux <florent@laas.fr>
2026-01-09 10:45:35 +0000

    [TransitionPlanner] Add comment.

--- include/hpp/manipulation/path-planner/transition-planner.hh
+++ include/hpp/manipulation/path-planner/transition-planner.hh
@@ -145,6 +145,9 @@ class HPP_MANIPULATION_DLLAPI TransitionPlanner : public core::PathPlanner {
   void clearPathOptimizers();
 
   /// Add a path optimizer
+  ///
+  /// \note The input path optimizer should have been constructed with the inner problem of this
+  /// class.
   void addPathOptimizer(const PathOptimizerPtr_t& pathOptimizer);
 
   /// Set parameter to the inner problem

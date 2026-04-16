2cb03e5a75b17330b0a78f2af81e33746a8b0f55
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-16 12:25:44 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,37 +1,27 @@
 {
   description = "Classes for manipulation planning";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    systems.follows = "gepetto/systems";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros.overrideAttrs.hpp-manipulation = _: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./doc
-                  ./include
-                  ./package.xml
-                  ./plugins
-                  ./src
-                  ./tests
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-manipulation = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./doc
+              ./include
+              ./package.xml
+              ./plugins
+              ./src
+              ./tests
+            ];
+          };
+        };
       }
     );
 }

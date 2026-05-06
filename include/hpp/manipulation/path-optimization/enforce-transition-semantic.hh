7191c818034f9fe8ce7a2240cbd0d5f0f0e08f15
Florent Lamiraux <florent@laas.fr>
2026-05-06 14:37:17 +0000

    [EnforceTransitionSemantics] Make class deprecated.
      This class was meant to postprocess paths planned by manipulation-RRT
      algorithm. The code has been included in the later planner, this makes the
      class useless.

--- include/hpp/manipulation/path-optimization/enforce-transition-semantic.hh
+++ include/hpp/manipulation/path-optimization/enforce-transition-semantic.hh
@@ -30,6 +30,7 @@
 #define HPP_MANIPULATION_PATHOPTIMIZATION_ENFORCE_TRANSITION_SEMANTIC_HH
 
 #include <hpp/core/path-optimizer.hh>
+#include <hpp/manipulation/deprecated.hh>
 #include <hpp/manipulation/problem.hh>
 
 namespace hpp {
@@ -63,6 +64,8 @@ class HPP_MANIPULATION_DLLAPI EnforceTransitionSemantic
  private:
   ProblemConstPtr_t problem_;
 };
+
+typedef EnforceTransitionSemantic ets HPP_MANIPULATION_DEPRECATED;
 
 /// \}
 }  // namespace pathOptimization

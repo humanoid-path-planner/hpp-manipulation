ca6161b2f06a429a09e04c773b892bc6c72e9598
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-08-17 15:11:02 +0200

    nix: switch to hpp-core devel
    https://github.com/humanoid-path-planner/hpp-core/pull/451 required
    since https://github.com/humanoid-path-planner/hpp-manipulation/pull/285

--- flake.nix
+++ flake.nix
@@ -1,13 +1,20 @@
 {
   description = "Classes for manipulation planning";
 
-  inputs.gepetto.url = "github:gepetto/nix";
+  inputs = {
+    gepetto.url = "github:gepetto/nix";
+    hpp-core = {
+      url = "github:humanoid-path-planner/hpp-core";
+      inputs.gepetto.follows = "gepetto";
+    };
+  };
 
   outputs =
     inputs:
     inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
+        overlays = [ inputs.hpp-core.overlays.flakoboros ];
         overrideAttrs.hpp-manipulation = {
           src = lib.fileset.toSource {
             root = ./.;
